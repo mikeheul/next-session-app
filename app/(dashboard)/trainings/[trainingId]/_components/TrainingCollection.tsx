77ffbcbbd173e8a6746296acad511e7dc48790a5
Micka <mickael.murmann@gmail.com>
2024-05-06 13:57:51 +0200

    TableRow onClick

---
+++ app/(dashboard)/trainings/[trainingId]/_components/TrainingCollection.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { formatDateTime } from "@/lib/format-datetime";
+import { Training } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
+interface TrainingCollectionProps {
+    trainings: any[]
+}
+
+const TrainingCollection = ({
+    trainings
+}: TrainingCollectionProps) => {
+
+    const router = useRouter()
+
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="">Training</TableHead>
+                    <TableHead>Sessions</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {trainings.map((training) => (
+                    <TableRow className="cursor-pointer" onClick={() => { router.push(`/trainings/${training.id}`) }} key={training.id}>
+                        <TableCell className="font-medium">{training.name}</TableCell>
+                        <TableCell>{training._count['sessions']}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
+}
+
+export default TrainingCollection;
--- app/(dashboard)/trainings/page.tsx
+++ app/(dashboard)/trainings/page.tsx
@@ -9,6 +9,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import TrainingCollection from "./[trainingId]/_components/TrainingCollection";
 
 const TrainingsPage = async () => {
     
@@ -33,22 +34,9 @@ const TrainingsPage = async () => {
         <div>
             <h1 className="text-2xl font-extrabold my-5">Trainings</h1>
 
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">Training</TableHead>
-                        <TableHead>Sessions</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {trainings.map((training) => (
-                        <TableRow key={training.id}>
-                            <TableCell className="font-medium"><Link href={`/trainings/${training.id}`}>{training.name}</Link></TableCell>
-                            <TableCell>{training._count['sessions']}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            <TrainingCollection 
+                trainings={trainings}
+            />
         </div>
     );
 }
