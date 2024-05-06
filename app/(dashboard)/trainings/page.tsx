ba11a52771495cc2ab114e0c38c12a2ac6ec2ed0
Micka <mickael.murmann@gmail.com>
2024-05-06 13:27:54 +0200

    Display trainings order by sessions count DESC

--- app/(dashboard)/trainings/page.tsx
+++ app/(dashboard)/trainings/page.tsx
@@ -1,14 +1,33 @@
 import { db } from "@/lib/db";
 import { Training } from "@prisma/client";
 import Link from "next/link";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 const TrainingsPage = async () => {
     
     const trainings = await db.training.findMany({
+        include: {
+            _count: {
+                select: { sessions: true }
+            }
+        },
         orderBy: {
-            name: 'asc'
-        }
+            sessions: {
+                _count: 'desc'
+            }
+        },
     });
+
+    console.log(trainings)
+
+    //trainings.sort((a, b) => b.sessions.length - a.sessions.length);
 
     if (!trainings) {
         throw new Error('Trainings not found');
@@ -17,13 +36,23 @@ const TrainingsPage = async () => {
     return (
         <div>
             <h1 className="text-2xl font-extrabold my-5">Trainings</h1>
-            {trainings.map((training: Training) => (
-                <div key={training.id}>
-                    <Link href={`/trainings/${training.id}`}>
-                        {training.name}
-                    </Link> 
-                </div>
-            ))}
+
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Training</TableHead>
+                        <TableHead>Sessions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {trainings.map((training) => (
+                        <TableRow key={training.id}>
+                            <TableCell className="font-medium"><Link href={`/trainings/${training.id}`}>{training.name}</Link></TableCell>
+                            <TableCell>{training._count['sessions']}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </div>
     );
 }
